66618584ecd844b272c7f12a18b4c7aea5f590ab
BlackIce <jhangirzafar@wardolph.com>
2015-02-01 14:24:03 +0000

    gitignore change

--- server/conf.js
+++ server/conf.js
@@ -2,9 +2,9 @@
 var config = {};
 config.twitter = {};
 
-config.twitter.consumer_key = 'xxx';
-config.twitter.consumer_secret = 'xxx';
-config.twitter.access_token = 'xxx';
-config.twitter.access_token_secret = 'xxx';
+config.twitter.consumer_key = 'KgcrIcoEErJuz2NJ56agDs5CB';
+config.twitter.consumer_secret = 'JYd1AUr8GsdsM1CsjIFkXbH4enkGoRDfSRxYTxWusV73KmTDKI';
+config.twitter.access_token = '62767825-6wRwMlYh5YnHrq13ChlkbFqy7La6wjXOBpnCuMPbU';
+config.twitter.access_token_secret = 'dkLxKZoKpcLAR7167p4SG76OxELP51MdZLuWyc6h8JRLZ';
 
 module.exports = config;
