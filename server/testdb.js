6e0c2dcf6b098dfdcec539c0cabb7b9e35c1b401
BlackIce <jhangirzafar@wardolph.com>
2015-02-01 15:32:07 +0000

    mongodb test file

---
+++ server/testdb.js
@@ -0,0 +1,31 @@
+var mongo = require('mongodb');
+
+var mongoClient = mongo.MongoClient;
+    
+
+
+// Connect to the db //strat mango db before trying to connect.
+mongoClient.connect("mongodb://"+config.ip+":27017/feminism", function(err, db) {
+      if(!err) {
+        console.log("mongodb: We are connected");
+        var collection = db.collection('tcollect');
+        
+        //collection.drop();
+        
+        
+        
+        var testPrint = function(){
+            console.log('testprint');
+            //collection.drop();
+            collection.find().toArray(function(err, items) {console.log(items);});
+ 
+        };
+        setInterval(testPrint, 5000);//clearing every 5 min
+        
+        
+        
+      }
+      else{
+          console.log("mongodb error connecting: "+err);
+      }
+});
