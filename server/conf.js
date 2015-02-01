6307451672a88d9af800cffda2f36f31d507a6c1
BlackIce <jhangirzafar@wardolph.com>
2015-02-01 15:09:43 +0000

    added config ip

--- server/conf.js
+++ server/conf.js
@@ -7,4 +7,6 @@ config.twitter.consumer_secret = 'xxx';
 config.twitter.access_token = 'xxx';
 config.twitter.access_token_secret = 'xxx';
 
+config.ip = '127.0.0.1';//local ip
+
 module.exports = config;
