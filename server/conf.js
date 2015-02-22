cf8dc238a30c32c996b22d0e53200c4f93538cd9
ubuntu-14 <jhangirzafar@wardolph.com>
2015-02-22 02:12:19 -0800

    added utils

--- server/conf.js
+++ server/conf.js
@@ -2,10 +2,10 @@
 var config = {};
 config.twitter = {};
 
-config.twitter.consumer_key = 'xxx';
-config.twitter.consumer_secret = 'xxx';
-config.twitter.access_token = 'xxx';
-config.twitter.access_token_secret = 'xxx';
+config.twitter.consumer_key = 'KgcrIcoEErJuz2NJ56agDs5CB';
+config.twitter.consumer_secret = 'JYd1AUr8GsdsM1CsjIFkXbH4enkGoRDfSRxYTxWusV73KmTDKI';
+config.twitter.access_token = '62767825-6wRwMlYh5YnHrq13ChlkbFqy7La6wjXOBpnCuMPbU';
+config.twitter.access_token_secret = 'dkLxKZoKpcLAR7167p4SG76OxELP51MdZLuWyc6h8JRLZ';
 
 config.ip = '127.0.0.1';//local ip
 
