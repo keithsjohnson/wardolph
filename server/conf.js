f76be43294ce091a91c7a72757b95315d599ed5c
BlackIce <jhangirzafar@wardolph.com>
2015-02-22 10:14:25 +0000

    Update conf.js

--- server/conf.js
+++ server/conf.js
@@ -2,10 +2,10 @@
 var config = {};
 config.twitter = {};
 
-config.twitter.consumer_key = 'KgcrIcoEErJuz2NJ56agDs5CB';
-config.twitter.consumer_secret = 'JYd1AUr8GsdsM1CsjIFkXbH4enkGoRDfSRxYTxWusV73KmTDKI';
-config.twitter.access_token = '62767825-6wRwMlYh5YnHrq13ChlkbFqy7La6wjXOBpnCuMPbU';
-config.twitter.access_token_secret = 'dkLxKZoKpcLAR7167p4SG76OxELP51MdZLuWyc6h8JRLZ';
+config.twitter.consumer_key = 'xxxx';
+config.twitter.consumer_secret = 'xxxx';
+config.twitter.access_token = 'xxxx';
+config.twitter.access_token_secret = 'xxxx';
 
 config.ip = '127.0.0.1';//local ip
 
